ade0eb701f00fb8cb8aa0a64ee994bfd5eeedd9e
mudroljub <mudroljub@gmail.com>
2024-12-24 20:34:01 +0100

    fix

--- src/scene/OtpisaniScena.js
+++ src/scene/OtpisaniScena.js
@@ -1,6 +1,4 @@
-// popraviti animaciju na osnovu mini-frp
-
-import UI from 'core/UI'
+// BUG: mrtav reaguje na pogodak
 import Scena from 'core/Scena'
 import Pozadina from 'core/Pozadina'
 import Okupator from '../2d-prvo-lice/Okupator'
@@ -9,36 +7,21 @@ import mish from 'io/mish'
 import slikaPozadina from 'slike/pozadine/rusevine-varsava.jpg'
 import zvukOtpisani from 'zvuci/otpisani.mp3'
 
-/*** KONFIG ***/
-
-const sablon = () => `
-<main class='centar'>
-  <h1>Ubij okupatora!</h1>
-  <p>Oslobođenje se bliži</p>
-</main>
-`
-
-/*** INIT ***/
-
-const pozadina = new Pozadina(slikaPozadina)
-const strazar = new Okupator()
-
-/*** EXPORT ***/
-
 export default class OtpisaniScena extends Scena {
   constructor(...args) {
     super(...args)
+    this.pozadina = new Pozadina(slikaPozadina)
+    this.strazar = new Okupator()
     mish.dodajNishan()
     this.zvuk = new Audio(zvukOtpisani)
     this.zvuk.play()
-    this.dodaj(new UI(sablon))
-    platno.addEventListener('click', () => strazar.proveriPogodak())
+    platno.addEventListener('click', () => this.strazar.proveriPogodak())
   }
 
   update() {
-    pozadina.update()
-    strazar.patroliraj()
-    strazar.update()
+    this.pozadina.update()
+    this.strazar.patroliraj()
+    this.strazar.update()
   }
 
   end() {
@@ -46,4 +29,13 @@ export default class OtpisaniScena extends Scena {
     mish.ukloniNishan()
     this.zvuk.pause()
   }
+
+  sablon() {
+    return `
+      <main class='centar'>
+        <h1>Ubij okupatora!</h1>
+        <p>Oslobođenje se bliži</p>
+      </main>
+    `
+  }  
 }
--- src/scene/avionce1944/Scena1944.js
+++ src/scene/avionce1944/Scena1944.js
@@ -1,7 +1,6 @@
 // ubaciti jednog neprijatelja i jednu stvar za hvatanje (paketić)
 // senku ispod aviona, kao u avion.png
 
-import UI from 'core/UI'
 import Scena from 'core/Scena'
 import {Avionce} from './Avionce'
 import {Okean} from './Okean'
@@ -9,33 +8,21 @@ import {Ostrvo} from './Ostrvo'
 import Oblak from './Oblak'
 import platno from 'io/platno'
 
-/*** KONFIG ***/
-
 let poeni = 0
 let zivoti = 3
 const oblaci = []
 const brojOblaka = 3
 const brzinaPozadine = 10
 
-
 const ostrvo = new Ostrvo(brzinaPozadine)
 const igrac = new Avionce()
 
 export default class Scena1944 extends Scena {
-  constructor() {
-    super()
-    const sablon = () => {
-      return `
-        <h1>Avionče 1944</h1>
-        Poeni: ${poeni}<br>
-        Životi: ${zivoti}<br>
-        Meci: ${igrac.preostaloMetaka()}
-      `
-    }
-    const interfejs = new UI(sablon)
+  constructor(...args) {
+    super(...args)
     const pozadina = new Okean(brzinaPozadine, platno.width)
     for (let i = 0; i < brojOblaka; i++) oblaci[i] = new Oblak(brzinaPozadine)
-    this.dodaj(pozadina, ostrvo, igrac, ...oblaci, interfejs)
+    this.dodaj(pozadina, ostrvo, igrac, ...oblaci)
   }
 
   update() {
@@ -54,6 +41,15 @@ export default class Scena1944 extends Scena {
         poeni++
       }
     })
+  }
+
+  sablon() {
+    return `
+      <h1>Avionče 1944</h1>
+      Poeni: ${poeni}<br>
+      Životi: ${zivoti}<br>
+      Meci: ${igrac.preostaloMetaka()}
+    `
   }
 
   end() {
