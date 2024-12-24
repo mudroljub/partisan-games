d86f90c2a92ea71a59328b814a35262c3ee6e472
mudroljub <mudroljub@gmail.com>
2024-12-24 20:56:05 +0100

    pomalo

--- src/scene/tenkici/TenkicIde.js
+++ src/scene/tenkici/TenkicIde.js
@@ -10,63 +10,60 @@ import Planina from 'src/2d-bocno/Planina'
 import Oblak from 'src/2d-bocno/Oblak'
 import platno from 'io/platno'
 
-/*** KONFIG ***/
-
 const BROJ_OBLAKA = 3
 const BROJ_ZBUNOVA = 10
 const PARALAX_1 = -5
-const zbunovi = []
-const oblaci = []
-const nivoTla = platno.height - 100
-
-const tenk = new TenkPartizanski(100, nivoTla)
-const planina = new Planina(nivoTla)
-const shumarak = new Shuma(nivoTla)
-
-shumarak.dx = PARALAX_1
-planina.dx = PARALAX_1
 
 export default class TenkicIde extends Scena {
   constructor(...args) {
     super(...args)
     this.init()
-    this.nivoTla = nivoTla
   }
-
+  
   init() {
+    this.nivoTla = platno.height - 100
+    this.tenk = new TenkPartizanski(100, this.nivoTla)
+    this.planina = new Planina(this.nivoTla)
+    this.shumarak = new Shuma(this.nivoTla)
+
+    this.shumarak.dx = PARALAX_1
+    this.planina.dx = PARALAX_1
+
+    this.zbunovi = []
+    this.oblaci = []
     for (let i = 0; i < BROJ_ZBUNOVA; i++) {
-      zbunovi[i] = new Zbun(nivoTla)
-      zbunovi[i].dx = PARALAX_1
+      this.zbunovi[i] = new Zbun(this.nivoTla)
+      this.zbunovi[i].dx = PARALAX_1
     }
     
     for (let i = 0; i < BROJ_OBLAKA; i++) {
-      oblaci[i] = new Oblak(150, 100)
-      oblaci[i].dx = PARALAX_1
+      this.oblaci[i] = new Oblak(150, 100)
+      this.oblaci[i].dx = PARALAX_1
     }    
   }
 
   azurirajZbunje() {
-    for (let i = 0; i < zbunovi.length; i++) {
-      zbunovi[i].update()
-      zbunovi[i].proveriGranice(10)
+    for (let i = 0; i < this.zbunovi.length; i++) {
+      this.zbunovi[i].update()
+      this.zbunovi[i].proveriGranice(10)
     }
   }
   
   azurirajOblake() {
-    for (let i = 0; i < oblaci.length; i++) {
-      oblaci[i].update()
-      oblaci[i].proveriGranice()
+    for (let i = 0; i < this.oblaci.length; i++) {
+      this.oblaci[i].update()
+      this.oblaci[i].proveriGranice()
     }
   }  
 
   update() {
-    this.crtaNeboZemlju(nivoTla)
-    planina.update()
-    shumarak.update()
-    shumarak.proveriGranice(platno.width / 2)
-    planina.proveriGranice(platno.width + 200)
+    this.crtaNeboZemlju(this.nivoTla)
+    this.planina.update()
+    this.shumarak.update()
+    this.shumarak.proveriGranice(platno.width / 2)
+    this.planina.proveriGranice(platno.width + 200)
     this.azurirajOblake()
-    tenk.update()
+    this.tenk.update()
     this.azurirajZbunje()
   }
 
@@ -79,8 +76,8 @@ export default class TenkicIde extends Scena {
        <br> W - gore
        <br> S - dole
        <br> space - puca
-       <div class="komande bg-poluprovidno energija1">${tenk.energija}</div>
-       <progress class="komande poluprovidno progres1" value="${tenk.energija}" max="100"></progress>
+       <div class="komande bg-poluprovidno energija1">${this.tenk.energija}</div>
+       <progress class="komande poluprovidno progres1" value="${this.tenk.energija}" max="100"></progress>
      </div>
     `
   }  
