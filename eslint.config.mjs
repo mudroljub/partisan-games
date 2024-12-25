411311ba7e49e790ea0ca5641fc9c23f4737de1a
mudroljub <mudroljub@gmail.com>
2024-12-25 17:30:52 +0100

    eslint init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,11 @@
+import globals from 'globals'
+
 export default [{
   languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+    },
 
     ecmaVersion: 'latest',
     sourceType: 'module',
