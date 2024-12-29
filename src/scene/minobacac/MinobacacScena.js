5efd0438089599a7f5beb395d7757fa35604170c
mudroljub <mudroljub@gmail.com>
2024-12-29 20:53:57 +0100

    pomalo

--- src/scene/minobacac/MinobacacScena.js
+++ src/scene/minobacac/MinobacacScena.js
@@ -1,17 +1,9 @@
-// predmet crta posredi, kvadrat od gore levo!
 // ubaciti sliku minobacača
 // bodovi, mozda pogoci prema pokušajima, mozda dva igraca
 
 import Scena from '/game-engine/core/Scena.js'
-import Predmet from '/game-engine/core/Predmet.js'
 import Kvadrat from '/game-engine/core/Kvadrat.js'
 import Minobacac from './Minobacac.js'
-
-/** * KONFIG ***/
-
-let brdo
-let minobacac
-let tlo
 
 export default class MinobacacScena extends Scena {
   constructor(...args) {
@@ -20,22 +12,21 @@ export default class MinobacacScena extends Scena {
   }
 
   init() {
-    brdo = new Predmet('/assets/slike/brdo.jpg', 85, 280, 500, 50)
-    minobacac = new Minobacac(10, 280, 200, 20)
-    tlo = new Kvadrat(0, 300, 600, 30, 'rgb(10,250,0)')
+    this.brdo = new Kvadrat(600, 0, 100, 250, 'black')
+    this.tlo = new Kvadrat(0, 300, 600, 30, 'green')
+    this.minobacac = new Minobacac(10, 280, 200, 20)
   }
 
   update() {
     this.cisti()
-    brdo.crta()
-    tlo.crta()
-    minobacac.update()
+    this.brdo.crta()
+    this.tlo.crta()
+    this.minobacac.update()
     this.proveriPogodak()
   }
 
   proveriPogodak() {
-    if (minobacac.projektil.sudara(brdo) || minobacac.projektil.sudara(tlo))
-      // TODO: reset()
-      this.end()
+    if (this.minobacac.projektil.sudara(this.brdo))
+      console.log('pogodak')
   }
 }
