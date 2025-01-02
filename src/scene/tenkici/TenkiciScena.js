2fc2ec8b00404d283a856b715d4f7db439dd435f
mudroljub <mudroljub@gmail.com>
2025-01-02 20:36:49 +0100

    m

--- src/scene/tenkici/TenkiciScena.js
+++ src/scene/tenkici/TenkiciScena.js
@@ -10,17 +10,41 @@ import Pozadina from '/game-engine/core/Pozadina.js'
 import TenkPartizanski from './TenkPartizanski.js'
 import TenkNemacki from './TenkNemacki.js'
 
-/** * INIT ***/
-
-const tenk1 = new TenkPartizanski(100, 450)
-const tenk2 = new TenkNemacki(650, 450)
-const pozadina = new Pozadina('/assets/slike/pozadine/razrusen-grad-savremen.jpg')
-
 export default class TenkiciScena extends Scena {
-  constructor() {
-    super()
-    this.velicina(800, 500)
-    this.dodaj()
+  init() {
+    this.tenk1 = new TenkPartizanski(100, 450)
+    this.tenk2 = new TenkNemacki(650, 450)
+    const pozadina = new Pozadina('/assets/slike/pozadine/razrusen-grad-savremen.jpg')
+    this.dodaj(pozadina)
+  }
+
+  proveriPogodak(granata, tenk, pomak) {
+    if (granata.sudara(tenk)) {
+      tenk.energija -= Math.round(Math.random() * 100)
+      granata.nestani()
+      tenk.x += pomak
+      if (tenk.energija <= 0) {
+        tenk.energija = 0
+        this.reset()
+      }
+    }
+  }
+
+  reset() {
+    const x1 = (Math.random() * platno.width / 2)
+    const x2 = (Math.random() * platno.width / 2) + platno.width / 2
+    this.tenk1.polozaj(x1, 450)
+    this.tenk2.polozaj(x2, 450)
+    this.tenk1.energija = this.tenk2.energija = 100
+  }
+
+  update() {
+    super.update()
+    this.tenk2.proveriTipke()
+    this.tenk1.update()
+    this.tenk2.update()
+    this.proveriPogodak(this.tenk1.granata, this.tenk2, 2)
+    this.proveriPogodak(this.tenk2.granata, this.tenk1, -2)
   }
 
   sablon() {
@@ -42,42 +66,10 @@ export default class TenkiciScena extends Scena {
       <br> ↓ dole
       <br> M - puca
     </div>
-    <div class='komande bg-poluprovidno energija1'>${tenk1.energija}</div>
-    <div class='komande bg-poluprovidno energija2'>${tenk2.energija}</div>
-    <progress class='komande poluprovidno progres1' value='${tenk1.energija}' max='100'></progress>
-    <progress class='komande poluprovidno progres2' value='${tenk2.energija}' max='100'></progress>
+    <div class='komande bg-poluprovidno energija1'>${this.tenk1.energija}</div>
+    <div class='komande bg-poluprovidno energija2'>${this.tenk2.energija}</div>
+    <progress class='komande poluprovidno progres1' value='${this.tenk1.energija}' max='100'></progress>
+    <progress class='komande poluprovidno progres2' value='${this.tenk2.energija}' max='100'></progress>
   `
   }
-
-  update() {
-    pozadina.render()
-    tenk2.proveriTipke()
-    tenk1.update()
-    tenk2.update()
-    proveriPogodak(tenk1.granata, tenk2, 2)
-    proveriPogodak(tenk2.granata, tenk1, -2)
-    this.renderSablon()
-  }
-}
-
-/** * POMOĆNE FUNKCIJE ***/
-
-function proveriPogodak(granata, tenk, pomak) {
-  if (granata.sudara(tenk)) {
-    tenk.energija -= Math.round(Math.random() * 100)
-    granata.nestani()
-    tenk.x += pomak
-    if (tenk.energija <= 0) {
-      tenk.energija = 0
-      reset()
-    }
-  }
-}
-
-function reset() {
-  const x1 = (Math.random() * platno.width / 2)
-  const x2 = (Math.random() * platno.width / 2) + platno.width / 2
-  tenk1.polozaj(x1, 450)
-  tenk2.polozaj(x2, 450)
-  tenk1.energija = tenk2.energija = 100
 }
