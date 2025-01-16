3947e660300c5affe73310d977ae1c8e16173125
mudroljub <mudroljub@gmail.com>
2025-01-16 20:55:28 +0100

    camac scena dodata obala

--- src/scene/CamacScena.js
+++ src/scene/CamacScena.js
@@ -1,10 +1,12 @@
 import Scena from '/game-engine/core/Scena.js'
 import CamacIgracOdozgo from '../2d-odozgo/CamacIgracOdozgo.js'
+import Obala from './Obala.js'
 
 export default class CamacScena extends Scena {
   init() {
     this.bojaPozadine = '#000066'
     const camac = new CamacIgracOdozgo()
-    this.dodaj(camac)
+    this.obala = new Obala()
+    this.dodaj(this.obala, camac)
   }
 }
---
+++ src/scene/Obala.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById('platno')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const lerp = (a, b, t) => a + (b - a) * t
+
+const random = opsegSuma => Math.random() * opsegSuma * 2 - opsegSuma
+
+export default class Obala {
+  constructor() {
+    this.noiseResolution = 100
+    this.opsegSuma = 25
+    this.sirinaReke = canvas.height - this.noiseResolution * .75
+    this.noisePoints = Array.from({ length: canvas.width / this.noiseResolution + 2 }, () => random(this.opsegSuma))
+    this.history = []
+  }
+
+  napred() {
+    this.history.push([...this.noisePoints])
+    this.noisePoints.shift()
+    this.noisePoints.push(random(this.opsegSuma))
+  }
+
+  nazad() {
+    if (this.history.length > 0)
+      this.noisePoints = this.history.pop()
+  }
+
+  render() {
+    for (let i = 0; i < canvas.width; i++) {
+      const index = Math.floor(i / this.noiseResolution)
+      const t = (i % this.noiseResolution) / this.noiseResolution
+      const offset = lerp(this.noisePoints[index], this.noisePoints[index + 1], t)
+
+      ctx.fillStyle = '#228B22'
+      ctx.fillRect(i, 0, 1, canvas.height / 2 + offset - this.sirinaReke / 2)
+      ctx.fillRect(i, canvas.height / 2 + offset + this.sirinaReke / 2, 1, canvas.height)
+    }
+  }
+}
