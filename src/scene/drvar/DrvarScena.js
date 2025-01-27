52c23008c4949e6f487bc9daa5f8a6659a43d584
mudroljub <mudroljub@gmail.com>
2025-01-27 11:04:27 +0100

    pomalo

--- src/scene/drvar/DrvarScena.js
+++ src/scene/drvar/DrvarScena.js
@@ -2,31 +2,33 @@ import { elements } from './data.js'
 import Predmet from '/game-engine/core/Predmet.js'
 import { kamera } from '/game-engine/core/Kamera.js'
 import Scena from '/game-engine/core/Scena.js'
-// import Panorama from './Panorama.js'
+import Panorama from './Panorama.js'
 
 const randSpread = range => range * (Math.random() - Math.random())
 
-// const pozadina = new Panorama()
-
+// TODO: naredba rendereru da crtaProjekciju
 export default class DrvarScena extends Scena {
   init() {
-    // this.sprites = []
-    const createSprites = el => {
-      for (let i = 0; i < el.number; ++i) {
-        const origin = el.origin ?? { x: 0, y: 0, z: 0 }
-        const range = el.range ?? { x: 10, y: 0, z: 10 }
-        const x = origin.x + randSpread(range.x)
-        const y = origin.y + randSpread(range.y)
-        const z = origin.z + randSpread(range.z)
-        this.predmeti.push(new Predmet(el.urls[i % el.urls.length], { x, y, z }))
-      }
-    }
-    elements.forEach(createSprites)
+    elements.forEach(el => {
+      for (let i = 0; i < el.number; ++i)
+        this.praviPredmet(el, i)
+    })
+    this.pozadina = new Panorama()
+  }
+
+  praviPredmet(el, i) {
+    const origin = el.origin ?? { x: 0, y: 0, z: 0 }
+    const range = el.range ?? { x: 10, y: 0, z: 10 }
+    const x = origin.x + randSpread(range.x)
+    const y = origin.y + randSpread(range.y)
+    const z = origin.z + randSpread(range.z)
+    const src = el.urls[i % el.urls.length]
+    this.predmeti.push(new Predmet(src, { x, y, z }))
   }
 
   render() {
+    this.pozadina.render()
     super.render()
-    // pozadina.render()
   }
 
   update(dt, t) {
