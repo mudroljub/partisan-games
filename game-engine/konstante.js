af83d74fd73164481ced2c8be00d7baaa7e7d821
mudroljub <mudroljub@gmail.com>
2025-01-27 12:30:02 +0100

    Object.freeze

--- game-engine/konstante.js
+++ game-engine/konstante.js
@@ -1,13 +1,13 @@
 export const KRUZNICA = Math.PI * 2
 
-export const ishodista = {
+export const ishodista = Object.freeze({
   centar: 'CENTAR',
   goreLevo: 'GORE_LEVO',
   doleDesno: 'DOLE_DESNO',
-}
+})
 
-export const naciniPrikaza = {
+export const naciniPrikaza = Object.freeze({
   oblik: 'OBLIK',
   slika: 'SLIKA',
   projekcija: 'PROJEKCIJA',
-}
+})
