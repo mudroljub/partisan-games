028af5868f6a3025d620d64b2431ed121050dfbd
mudroljub <mudroljub@gmail.com>
2025-01-31 12:02:40 +0100

    SceneManager

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -1,19 +1,44 @@
-import { loadScene } from './scene.js'
+const putanje = {
+  BombasScena: './scene/bombas/BombasScena.js',
+  NemciIzRovova: './scene/rovovi/NemciIzRovova.js',
+  Scena1942: './scene/avionce1942/Scena1942.js',
+  CamacScena: './scene/CamacScena.js',
+  OtpisaniScena: './scene/OtpisaniScena.js',
+  Scena1944: './scene/avionce1944/Scena1944.js',
+  TopScena: './scene/top/TopScena.js',
+  TenkicIde: './scene/tenkic-ide/TenkicIde.js',
+  TenkiciScena: './scene/tenkici/TenkiciScena.js',
+  RanjenikScena: './scene/ranjenik/RanjenikScena.js',
+  DrvarScena: './scene/drvar/DrvarScena.js',
+  JasenovacScena: './scene/tenkovska/JasenovacScena.js',
+  TenkOdozgoScena: './scene/tenkovska/TenkOdozgoScena.js',
+  GlavniMeni: './GlavniMeni.js'
+}
 
-export default class SceneManager {
+class SceneManager {
+  static instance = null
+
   constructor() {
-    if (SceneManager.instance) return SceneManager.instance
+    if (SceneManager.instance)
+      return SceneManager.instance
+
     this.currentScene = null
     SceneManager.instance = this
   }
 
-  start(key) {
+  async loadScene(ime) {
+    const sceneModule = await import(putanje[ime])
+    return sceneModule.default
+  }
+
+  async start(ime) {
     if (this.currentScene)
       this.currentScene.end()
 
-    loadScene(key).then(Scena => {
-      this.currentScene = new Scena(this)
-      this.currentScene.start()
-    })
+    const SceneClass = await this.loadScene(ime)
+    this.currentScene = new SceneClass(this)
+    this.currentScene.start()
   }
 }
+
+export default SceneManager
