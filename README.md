84fbe0a63a56ad9aa3afc673c605070d895db159
mudroljub <mudroljub@gmail.com>
2025-02-17 00:18:11 +0100

    - x ne moze continue

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ Repo: [github.com/partisan-games/partisan-games.github.io](https://github.com/pa
 
 ### UI
 
-- Spomenici i ostali FPS ne briše zdravlje po izlasku
 - meni vratiti burger?
+- Spomenici i ostali FPS ne briše zdravlje po izlasku
 - za uvodni ekran staviti neku art pozadinu?
 - drugačiji završni ekran za pobedu i poraz
 
--- ui/UI.js
+++ ui/UI.js
@@ -28,7 +28,7 @@ export default class UI {
     return /* html */`
       <div class="central-screen simple-container" id="start-screen">
         <p>${this.intro}</p>
-        <button id="start"><span>⚔️</span> To battle</button>
+        <button id="start"><span>★</span> To battle</button>
       </div>
     `
   }
@@ -38,8 +38,8 @@ export default class UI {
     return /* html */`
       <div class="central-screen simple-container game-paused">
         <p>Game paused</p>
-        <button id="continue"><span>⚔️</span> Continue</button>
-        <button id="menu"><span>↩</span> Main menu</button>
+        <button id="continue"><span>★</span> Continue</button>
+        <button id="menu"><span>☰</span> Main menu</button>
         <button id="igraj-opet"><span>↻</span> Play again</button>
       </div>
     `
@@ -49,7 +49,7 @@ export default class UI {
     return /* html */`
       <div class="central-screen simple-container">
         <p>${this.outro}</p>
-        <button id="menu"><span>↩</span> Main menu</button>
+        <button id="menu"><span>☰</span> Main menu</button>
         <button id="igraj-opet"><span>↻</span> Play again</button>
       </div>
     `
