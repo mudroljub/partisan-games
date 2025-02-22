bf95c7dc31caae7084a9d83b9aaffc126d6d6ebd
mudroljub <mudroljub@gmail.com>
2025-02-22 21:36:43 +0100

    m

--- scenes/MainMenu.js
+++ scenes/MainMenu.js
@@ -9,22 +9,16 @@ const icons = [
   'buildings/crkva-01.png',
   'buildings/kuca-bunker.png',
   'buildings/kula-01.png',
-  'buildings/ruina-03.png',
-  'items/bodljikava-zica.gif',
-  'slicice/avionce.gif',
-  'slicice/camac.png',
+  'slicice/spitfire.png',
   'slicice/cetnici/cetnik-03.png',
   'slicice/figurica-brka.png',
   'slicice/hummel.png',
-  'slicice/nemci/artiljerija/top-01.png',
-  'slicice/nemci/avioni/avion-03.png',
   'slicice/nemci/avioni/fieseler-fi-156-storch-03.png',
   'slicice/nemci/avioni/focke-wulf-fw-190.png',
   'slicice/nemci/avioni/fw-190f-8.png',
   'slicice/nemci/avioni/junkers-ju-52.png',
   'slicice/nemci/avioni/junkers-ju87b-stuka.png',
   'slicice/nemci/avioni/junkers-ju87d-stuka-03.png',
-  'slicice/nemci/padobranci/padobranac-01.png',
   'slicice/nemci/veza-02.png',
   'slicice/nemci/vojnici/juris-01.png',
   'slicice/nemci/vojnici/narednik-01.png',
@@ -33,9 +27,7 @@ const icons = [
   'slicice/partizani/bolnicari/bolnicarka-02.png',
   'slicice/partizani/lokomotiva-01.png',
   'slicice/partizani/marshal-tito.png',
-  'slicice/partizani/potez-25.png',
   'slicice/partizani/tenkovi/tenk-04.png',
-  'slicice/partizani/vojnici/bombasi/partizan-bombas.gif',
   'slicice/partizani/vojnici/komesar-01.png',
   'slicice/partizani/vojnici/komesar-02.png',
   'slicice/partizani/vojnici/mp40-05.png',
@@ -52,7 +44,6 @@ const icons = [
   'slicice/partizani/vojnici/partizan-14.png',
   'slicice/partizani/vojnici/partizan-17.png',
   'slicice/partizani/vojnici/partizan-18.png',
-  'slicice/partizani/vojnici/partizan-21.png',
   'slicice/partizani/vojnici/partizan-24.png',
   'slicice/partizani/vojnici/partizan-25.png',
   'slicice/partizani/vojnici/partizan-28.png',
@@ -60,23 +51,21 @@ const icons = [
   'slicice/partizani/vojnici/stoji-02.png',
   'slicice/partizani/vozila/dzip-01.png',
   'slicice/rusi/vojnici/bombas-01.png',
-  'slicice/rusi/vojnici/bombas-04.png',
   'slicice/rusi/vojnici/vojnik-08.png',
   'slicice/rusi/vojnici/zastavnik-01.png',
   'slicice/rusi/vojnici/zene/borkinja-02.png',
   'slicice/rusi/vojnici/zene/borkinja-04.png',
-  'slicice/spitfire.png',
 ]
 
 shuffle(icons)
 const pozicije = slucajnePozicije(Object.keys(scenes).length, 140)
 
-const renderIcon = (key, value, i) => {
+const renderIcon = (key, data, i) => {
   const style = `"top: ${pozicije[i].y}px; left: ${pozicije[i].x}px;"`
   return /* html */`
     <button value='${key}' class='menu-btn js-start' style=${style}>
-    ${value.name}<br>
-      <img src="/assets/images/${icons[i]}" height="40">
+    <img src="/assets/images/${data.icon || icons[i]}" height="${data.height || 40}">
+    <br>${data.name}
     </button>
   `
 }
@@ -107,8 +96,8 @@ export default class MainMenu extends Scena2D {
   handleVisibilityChange() {}
 
   sceneUI() {
-    // <h1>Partisan Games ★</h1>
     return `
+      <h1>Partisan Games ★</h1>
       ${this.manuIcons}
     `
   }
--- scenes/scenes.js
+++ scenes/scenes.js
@@ -1,22 +1,98 @@
 export const scenes = {
-  SpomeniciScena: { path: './spomenici/SpomeniciScena.js', name: 'Spomenici' },
-  BeogradScena: { path: './beograd/BeogradScena.js', name: 'Liberation of Belgrade' },
-  TenkScena: { path: './tenk/TenkScena.js', name: 'Tenk' },
-  RatweekScena: { path: './warplane/RatweekScena.js', name: 'Operation Ratweek' },
-  RajlovacScena: { path: './rajlovac/RajlovacScena.js', name: 'Attack on Rajlovac Airport' },
-  SavoScena: { path: './savo/SavoScena.js', name: 'Savo Mitraljezac' },
-  KrupanjScena: { path: './krupanj/KrupanjScena.js', name: 'Battle of Krupanj' },
-  NemciIzRovova: { path: './rovovi/NemciIzRovova.js', name: 'Nemci iz rovova' },
-  FranjoKluzScena: { path: './franjo-kluz/FranjoKluzScena.js', name: 'Franjo Kluz' },
-  DrinaScena: { path: './drina/DrinaScena.js', name: 'Crossing the Drina' },
-  OtpisaniScena: { path: './OtpisaniScena.js', name: 'Otpisani scena' },
-  Scena1944: { path: './avionce1944/Scena1944.js', name: 'Avionče 1944' },
-  TopScena: { path: './top/TopScena.js', name: 'Top' },
-  TenkicIde: { path: './tenkic-ide/TenkicIde.js', name: 'Tenkić ide' },
-  TenkiciScena: { path: './tenkici/TenkiciScena.js', name: 'Tenkići' },
-  RanjenikScena: { path: './ranjenik/RanjenikScena.js', name: 'Ranjenik na Sutjesci' },
-  DrvarScena: { path: './drvar/DrvarScena.js', name: 'Raid on Drvar' },
-  JasenovacScena: { path: './tenkovska/JasenovacScena.js', name: 'Bekstvo iz Jasenovca' },
-  TenkOdozgoScena: { path: './tenkovska/TenkOdozgoScena.js', name: 'Tenk odozgo' },
-  MainMenu: { path: './MainMenu.js', name: 'Main Menu' }
+  SpomeniciScena: {
+    path: './spomenici/SpomeniciScena.js',
+    name: 'Spomenici'
+  },
+  BeogradScena: {
+    path: './beograd/BeogradScena.js',
+    name: 'Liberation of Belgrade',
+    icon: 'slicice/partizani/vojnici/partizan-21.png',
+  },
+  TenkScena: {
+    path: './tenk/TenkScena.js',
+    name: 'Tenk'
+  },
+  RatweekScena: {
+    path: './warplane/RatweekScena.js',
+    name: 'Operation Ratweek'
+  },
+  RajlovacScena: {
+    path: './rajlovac/RajlovacScena.js',
+    name: 'Attack on Rajlovac Airport',
+    icon: 'slicice/nemci/avioni/avion-03.png',
+  },
+  SavoScena: {
+    path: './savo/SavoScena.js',
+    name: 'Savo Mitraljezac'
+  },
+  KrupanjScena: {
+    path: './krupanj/KrupanjScena.js',
+    name: 'Battle of Krupanj',
+    icon: 'slicice/partizani/vojnici/bombasi/partizan-bombas.gif',
+    height: 48
+  },
+  NemciIzRovova: {
+    path: './rovovi/NemciIzRovova.js',
+    name: 'Nemci iz rovova'
+  },
+  FranjoKluzScena: {
+    path: './franjo-kluz/FranjoKluzScena.js',
+    name: 'Franjo Kluz',
+    icon: 'slicice/partizani/potez-25.png',
+  },
+  DrinaScena: {
+    path: './drina/DrinaScena.js',
+    name: 'Crossing the Drina',
+    icon: 'slicice/camac.png',
+  },
+  OtpisaniScena: {
+    path: './OtpisaniScena.js',
+    name: 'Otpisani scena',
+    icon: 'buildings/ruina-03.png',
+  },
+  Scena1944: {
+    path: './avionce1944/Scena1944.js',
+    name: 'Avionče 1944',
+    icon: 'slicice/avionce.gif',
+  },
+  TopScena: {
+    path: './top/TopScena.js',
+    name: 'Top',
+    icon: 'slicice/nemci/artiljerija/top-01.png',
+    height: 32
+  },
+  TenkicIde: {
+    path: './tenkic-ide/TenkicIde.js',
+    name: 'Tenkić ide'
+  },
+  TenkiciScena: {
+    path: './tenkici/TenkiciScena.js',
+    name: 'Tenkići'
+  },
+  RanjenikScena: {
+    path: './ranjenik/RanjenikScena.js',
+    name: 'Ranjenik na Sutjesci',
+    icon: 'slicice/ranjeni-partizan.png',
+    height: 21
+  },
+  DrvarScena: {
+    path: './drvar/DrvarScena.js',
+    name: 'Raid on Drvar',
+    icon: 'slicice/nemci/padobranci/padobranac-01.png',
+    height: 90
+  },
+  JasenovacScena: {
+    path: './tenkovska/JasenovacScena.js',
+    name: 'Bekstvo iz Jasenovca',
+    icon: 'items/bodljikava-zica.gif',
+    height: 32
+  },
+  TenkOdozgoScena: {
+    path: './tenkovska/TenkOdozgoScena.js',
+    name: 'Tenk odozgo'
+  },
+  MainMenu: {
+    path: './MainMenu.js',
+    name: 'Main Menu'
+  }
 }
